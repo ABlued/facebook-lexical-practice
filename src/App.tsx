e6381764db6fe053d67536f174ee1d52269aef22
NaJaeWan <2015132011@kpu.ac.kr>
2023-10-31 23:12:45 +0900

    feat : implement history Plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,66 @@
 import React, { useMemo } from 'react';
-import {InitialConfigType, LexicalComposer} from '@lexical/react/LexicalComposer';
-import {PlainTextPlugin} from "@lexical/react/LexicalPlainTextPlugin";
-import {ContentEditable} from '@lexical/react/LexicalContentEditable';
+import {
+  InitialConfigType,
+  LexicalComposer,
+} from '@lexical/react/LexicalComposer';
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
+import { ContentEditable } from '@lexical/react/LexicalContentEditable';
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
-const App :React.FC = () => {
-
-
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
+import { CustomHistoryActions } from './components';
+import { OnChangePlugin } from './components/OnChangePlugin/OnChangePlugin';
+const App: React.FC = () => {
   const CustomContent = useMemo(() => {
     return (
-        <ContentEditable style={{
-            position: 'relative',
-            borderColor: 'rgba(255,211,2,0.68)',
-            border: '2px solid red',
-            borderRadius: '5px',
-            maxWidth: '100%',
-            padding: '10px'
-        }}/>
-    )
-}, []);
+      <ContentEditable
+        style={{
+          position: 'relative',
+          borderColor: 'rgba(255,211,2,0.68)',
+          border: '2px solid red',
+          borderRadius: '5px',
+          maxWidth: '100%',
+          padding: '10px',
+        }}
+      />
+    );
+  }, []);
 
-
-const CustomPlaceholder = useMemo(() => {
-  return (
-      <div style={{
-          position: 'absolute', top: 30, left: 30,
-      }}>
-          Enter some text...
+  const CustomPlaceholder = useMemo(() => {
+    return (
+      <div
+        style={{
+          position: 'absolute',
+          top: 30,
+          left: 30,
+        }}
+      >
+        Enter some text...
       </div>
-  )
-}, []);
+    );
+  }, []);
 
-const lexicalConfig: InitialConfigType = {
-  namespace: 'My Rich Text Editor',
-  onError: (e) => {
-      console.log('ERROR:', e)
-  }
-}
-return (
-  <div style={{padding: '20px'}}>
+  const lexicalConfig: InitialConfigType = {
+    namespace: 'My Rich Text Editor',
+    onError: (e) => {
+      console.log('ERROR:', e);
+    },
+  };
+  return (
+    <div style={{ padding: '20px' }}>
       <LexicalComposer initialConfig={lexicalConfig}>
-          <PlainTextPlugin
-              contentEditable={CustomContent}
-              placeholder={CustomPlaceholder}
-              ErrorBoundary={LexicalErrorBoundary}
-          />
+        <RichTextPlugin
+          contentEditable={CustomContent}
+          placeholder={CustomPlaceholder}
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+        <HistoryPlugin />
+        <OnChangePlugin />
+        <div style={{ margin: '20px 0px' }}>
+          <CustomHistoryActions />
+        </div>
       </LexicalComposer>
-  </div>
-);
-}
+    </div>
+  );
+};
 
 export default App;
